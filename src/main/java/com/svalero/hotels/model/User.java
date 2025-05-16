0034902e1724bcf4c4fa6c01459dc8e089d64777
Borja <borzg2000@gmail.com>
2025-05-16 15:38:15 +0200

    Modelos terminados

--- src/main/java/com/svalero/hotels/model/User.java
+++ src/main/java/com/svalero/hotels/model/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class User {
     private int idUser;
+    private String username;
     private String firstName;
     private String lastName;
     private String dni;
@@ -14,8 +15,9 @@ public class User {
 
     public User() {}
 
-    public User(int userId, String firstName, String lastName, String dni, String email, String phone, String password, String role) {
-        this.userId = userId;
+    public User(int idUser, String username, String firstName, String lastName, String dni, String email, String phone, String password, String role) {
+        this.userId = idUser;
+        this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.dni = dni;
@@ -25,5 +27,40 @@ public class User {
         this.role = role;
     }
 
+    public int getIdUser() {return idUser;}
+
+    public void setIdUser(int idUser) {this.idUser = idUser;}
+
+    public String getUsername() {return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public String getDni() {return dni;}
+
+    public void setDni(String dni) {this.dni = dni;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPhone() {return phone;}
+
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public String getRole() {return role;}
+
+    public void setRole(String role) {this.role = role;}
 
 }
